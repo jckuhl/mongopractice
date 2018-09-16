d1d42106142cbe8c13aed4b1a7160ad2934f40e5
Jonathan Kuhl <prj.breakpoint@gmail.com>
2018-09-16 19:49:19 -0400

    Add files via upload
    Added Delete operation

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const get = async (url, params={})=> (await fetch(url, params)).json();
-const post = (url, params={})=> fetch(url, params);
+const ajax = (url, params={})=> fetch(url, params);
 
-const ul = document.getElementById('ul');
-
-function constructList(object) {
+function constructList(object, ul) {
     const li = document.createElement('li');
     for(let prop in object) {
         if(object.hasOwnProperty(prop) && prop !== '_id') {
@@ -17,11 +15,13 @@ function constructList(object) {
 document.getElementById('get-general').addEventListener('click', (event)=> {
     event.preventDefault();
 
+    const ul = document.getElementById('ul');
+
     fetch('http://localhost:3000/generals')
         .then(data=> data.json())
         .then(json=> {
             ul.innerHTML = '';
-            json.forEach(general=> constructList(general))
+            json.forEach(general=> constructList(general, ul))
         })
         .catch(error=> console.error(error));
 });
@@ -34,11 +34,73 @@ document.getElementById('submit').addEventListener('click', event => {
         side: document.getElementById('side').value
     }
 
-    post('http://localhost:3000/generals', {
+    ajax('http://localhost:3000/generals', {
         method: 'POST',
         body: JSON.stringify(general),
         headers: {
             'Content-Type': 'application/json'
         }
     });
-})
+});
+
+document.getElementById('search').addEventListener('click', event => {
+    event.preventDefault();
+
+    const name = document.getElementById('sname').value;
+    const side = document.getElementById('sside').value;
+    const queryName = `name=${name}`;
+    const querySide = `side=${side}`;
+    let url = 'http://localhost:3000/generals/search';
+
+    if(name && !side) {
+        url += '?' + queryName;
+    } else if(name && side) {
+        url += '?' + queryName + '&' + querySide;
+    } else if(!name && side) {
+        url += '?' + querySide;
+    }
+
+    const result = document.getElementById('result');
+
+    fetch(url)
+        .then(data => data.json())
+        .then(json => {
+            result.innerHTML = '';
+            json.forEach(general => constructList(general, result));
+        })
+        .catch(error => console.error(error))
+});
+
+(function() {
+    const tbody = document.querySelector('tbody');
+
+    function constructTable(general) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${general.name}</td>
+            <td>${general.side}</td>
+            <td><button>Update</button></td>
+            <td><button data-id=${general._id}>Delete</button></td>`;
+        tbody.appendChild(tr);
+
+        document.querySelector(`button[data-id="${general._id}"]`).addEventListener('click', event => {
+            event.preventDefault();
+            const id = event.target.dataset.id;
+            ajax('http://localhost:3000/generals/delete', {
+                method: 'DELETE',
+                body: JSON.stringify({ _id: id }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        });
+    }
+
+    fetch('http://localhost:3000/generals')
+        .then(data => data.json())
+        .then(json => {
+            json.forEach(general => constructTable(general));
+        })
+        .catch(error => console.error(error));
+
+})();
--- test.js
+++ test.js
@@ -1,14 +1,23 @@
+// Bring in dependencies
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+// Start the Express app.
 const app = express();
 
+// Construct the Mongo connection string with pw and username stored in environment
 const user = process.env.MONGO_USER;
 const pw = process.env.MONGO_PW;
 const uri = `mongodb+srv://${user}:${pw}@cluster0-vji0s.mongodb.net/test?retryWrites=true`;
 
-function readAll(query={}) {
+/**
+ * Querys the database for information
+ * @param {Object} query A MongoDB query applicable for the .find() method
+ */
+function read(query={}) {
     return new Promise((resolve, reject)=> {
         MongoClient.connect(uri, {useNewUrlParser: true }, function(err, client) {
             if(err) {
@@ -31,7 +40,10 @@ function readAll(query={}) {
     })
 }
 
-
+/**
+ * Inserts a general into the general database.
+ * @param {Object} general Any General object created by the form
+ */
 function insert(general) {
     MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
         const collection = client.db("first-test").collection("users");
@@ -40,22 +52,54 @@ function insert(general) {
     });
 }
 
+/**
+ * Removes the general with the given ID
+ * @param {Object} id Shape: { _id: ObjectId("id hash")}
+ */
+function remove(id) {
+    MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
+        const collection = client.db("first-test").collection("users");
+        collection.deleteOne(id);
+        client.close();
+    });
+}
+
+// Middle ware, CORS and body-parser
 app.use(cors());
 app.use(bodyParser.json());
 
+// Set the /generals route for GET requests to retrieve data
 app.get('/generals', (request, response)=> {
-    readAll().then(data => {
-        console.log(data);
+    read().then(data => {
         response.send(JSON.stringify(data));
     });
 });
 
+app.get('/generals/search', (request, response)=> {
+    read(request.query).then(data => {
+        response.send(JSON.stringify(data));
+    });
+});
+
+// Set the /generals route for POST requests to insert data
 app.post('/generals', (request, response)=> {
-    console.log(request.body);
     insert(request.body);
     response.sendStatus(200);
 });
 
+// Set the /generals/delete route for DELETE requests to remove data
+// Okay for this you'd want some added security, but this is just for funzies.
+app.delete('/generals/delete', (request, response)=> {
+
+    const query = {
+        _id: new ObjectID(request.body._id)
+    }
+
+    remove(query);
+    response.sendStatus(200);
+});
+
+// Start the Express server
 app.listen(3000, ()=> {
     console.log('listening on port 3000');
 });
